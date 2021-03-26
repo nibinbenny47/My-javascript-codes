fb765306ef2562ed1668085726d18f29979596e2
nibinbenny47 <75657897+nibinbenny47@users.noreply.github.com>
2021-03-26 20:56:34 +0530

    Select all rows by clicking one checkbox at the top

---
+++ Javascript/SelectallDataByCheckbox.aspx.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+using System.IO;
+public partial class Guest_Default : System.Web.UI.Page
+{
+    SqlConnection con = new SqlConnection("Data Source=.;Initial Catalog=db_myproject;Integrated Security=True");
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        con.Open();
+        if (!IsPostBack)
+        {
+            BindGrid();
+        }
+    }
+    protected void RowDataBound(object sender, GridViewRowEventArgs e)
+
+    {
+
+        if (e.Row.RowType == DataControlRowType.DataRow)
+
+        {
+
+            e.Row.Attributes.Add("onmouseover", "MouseEvents(this, event)");
+
+            e.Row.Attributes.Add("onmouseout", "MouseEvents(this, event)");
+
+        }
+
+    }
+    protected void BindGrid()
+    {
+        string selQry = "select * from tbl_state";
+        SqlDataAdapter adp = new SqlDataAdapter(selQry, con);
+
+        DataTable dt = new DataTable();
+        adp.Fill(dt);
+        GridView1.DataSource = dt;
+        GridView1.DataBind();
+    }
+}
