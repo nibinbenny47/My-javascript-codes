52f1cb3336950598dbfe0b9345d8591e5f9b7b5f
nibinbenny47 <75657897+nibinbenny47@users.noreply.github.com>
2021-03-30 20:12:05 +0530

    Exporting and importing functions

---
+++ Javascript/Calc.js
@@ -0,0 +1,16 @@
+ export function sum(a,b){
+    return a+b;
+}
+ export  function mul(a,b){
+    return a*b;
+    
+}
+ function sub(a,b){
+    return a-b;
+}
+ function div(a,b){
+    return a/b;
+
+}
+/*****we can export functions together at the end 
+export {sum,mul};*/
